fcef0e7a1b3f3887b9c110ee7c00f5a05870be49
Zhahnur Arif <mzhahnur@gmail.com>
2023-11-12 07:44:03 +0700

    Screen 3 Arif uhuyy

--- screens/Invoice.js
+++ screens/Invoice.js
@@ -1,45 +1,140 @@
 import React from "react";
+import {
+  Button,
+  Box,
+  Stack,
+  Text,
+  Divider,
+  HStack,
+  Heading,
+  Image,
+} from "native-base";
 import { useNavigation } from "@react-navigation/native";
-import { Button, Box, VStack, Text, Divider } from "native-base";
-import { Header } from "../components";
 
 const Invoice = () => {
   const navigation = useNavigation();
 
   return (
     <Box>
-      <Header title="Invoice" />
-      <Box borderWidth={1} borderColor="#ccc" borderRadius={5} padding={10} marginBottom={20}>
-        <VStack space={2}>
-          <Text bold>Nama:</Text>
-          <Text>Muhammad Zhahnur Arif</Text>
+      <Box
+        py={3}
+        shadow={2}
+        borderRadius="md"
+        margin={10}
+        mb={10}
+        ml={3}
+        mr={3}
+        mt={10}
+      >
+        <Image
+          source={require("../assets/icon.png")}
+          alt="Icon"
+          width={75}
+          height={75}
+          alignSelf="center"
+          mt={3}
+        />
+        <Heading
+          fontSize="lg"
+          fontWeight="bold"
+          textAlign="center"
+          mt={2}
+          className="invoice-heading"
+        >
+          Struk Transaksi
+        </Heading>
+        <Divider mt={5} />
+        <Stack space={3} w="90%" mx="auto" mt={4}>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "center" }}>
+              192959127501218512
+            </Text>
+          </HStack>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Nama
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>
+              Muhammad Zhahnur Arif
+            </Text>
+          </HStack>
 
-          <Text bold>Nomor HP:</Text>
-          <Text>082140715463</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Nomor HP
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>082140715463</Text>
+          </HStack>
 
-          <Text bold>Alamat:</Text>
-          <Text>Sidoarjo</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Alamat
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>
+              Sidoarjo - Jawa Timur
+            </Text>
+          </HStack>
 
-          <Text bold>Jenis Layanan:</Text>
-          <Text>Cuci Basah</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Jenis Layanan
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>Cuci Setrika</Text>
+          </HStack>
 
-          <Text bold>Tanggal Pesanan:</Text>
-          <Text>10 April 2002</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Tanggal Pesanan
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>
+              15 November 2023
+            </Text>
+          </HStack>
 
-          <Text bold>Berat (kg):</Text>
-          <Text>10</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Berat
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>10 kg</Text>
+          </HStack>
 
-          <Text bold>Keterangan:</Text>
-          <Text>Baju Batik, Baju Sekolah, Baju Pramuka</Text>
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "left" }}>
+              Harga
+            </Text>
+            <Text style={{ flex: 1, textAlign: "right" }}>Rp35.000</Text>
+          </HStack>
 
+          <HStack alignItems="center">
+            <Text bold style={{ flex: 1, textAlign: "center" }}>
+              Keterangan
+            </Text>
+          </HStack>
+          <HStack alignItems="center">
+            <Text style={{ flex: 1, textAlign: "center" }}>
+              Baju Batik, Baju Sekolah, Baju Pramuka
+            </Text>
+          </HStack>
           <Divider />
-
-          <Text bold>Harga:</Text>
-          <Text>Rp35.000</Text>
-        </VStack>
-        <Button onPress={() => navigation.navigate('Home')} width="auto">
-        OK
-      </Button>
+          <HStack alignItems="center">
+            <Text style={{ flex: 1, textAlign: "center" }}>
+              SIMPAN STRUK TRANSAKSI INI
+            </Text>
+          </HStack>
+          <HStack alignItems="center">
+            <Text style={{ flex: 1, textAlign: "center" }}>
+              STRUK INI MERUPAKAN BUKTI PEMBAYARAN SAH
+            </Text>
+          </HStack>
+          <HStack alignItems="center">
+            <Text style={{ flex: 1, textAlign: "center" }}>
+              TANGGAL TERBIT 12/11/2023 07.28
+            </Text>
+          </HStack>
+          <Box alignItems="center" mt={2}>
+            <Button mb={2} onPress={() => navigation.navigate("Home")}>OKE</Button>
+          </Box>
+        </Stack>
       </Box>
     </Box>
   );

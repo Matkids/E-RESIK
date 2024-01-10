2fb5cd15a1416e0f2adfb41d32cd8ddf2def5555
TheSupri_Cat <132975405+Ardan1007@users.noreply.github.com>
2024-01-10 20:46:58 +0700

    PunyaFarrelTerbaru

--- screens/RincianPesanan.js
+++ screens/RincianPesanan.js
@@ -23,7 +23,7 @@ const RincianPesanan = ({ route }) => {
   useEffect(() => {
     // Gantilah "NamaPesanan" dengan referensi yang sesuai di Firebase
     const pesananRef = ref(getDatabase(), `Pesanan/${pesananId}`);
-    
+
     // Menggunakan onValue untuk mendapatkan data secara real-time
     onValue(pesananRef, (snapshot) => {
       const data = snapshot.val();
@@ -37,69 +37,70 @@ const RincianPesanan = ({ route }) => {
     <>
       {/* Menggunakan komponen Header untuk judul halaman */}
       <Header title={"Rincian Pesanan"} withBack="true" />
-      {/* Menggunakan SafeAreaView untuk memastikan tata letak aman pada perangkat dengan notch */}
-      <SafeAreaView flex={1}>
-        {/* ScrollView untuk menggulirkan konten jika lebih panjang dari layar */}
-        <ScrollView flex={1}>
-          {/* VStack untuk menumpuk elemen-elemen secara vertikal */}
-          <VStack px={5} pt={6}>
-            {dataPesanan && (
-              <Box
-                bg={"#FFFF"}
-                rounded="xl"
-                overflow="hidden"
-                shadow={4}
-                borderColor="#0878CA"
-                borderWidth="1"
-                backgroundColor="#FFFF"
-                pt={2}
-                pr={4}
-                pb={2}
-                pl={4}
-                mb={3}
-              >
-                {/* Box untuk memberi margin dan padding */}
-                <Box ml={2} m={2}>
-                  {/* Text bold untuk menampilkan teks dengan tebal */}
-                  <Text bold>{dataPesanan.nama}</Text>
-                </Box>
-                {/* Divider untuk membuat garis pemisah */}
-                <Divider />
-                
-                {/* HStack untuk menyusun elemen secara horizontal */}
-                <HStack px={2} py={2}>
-                  {/* Image untuk menampilkan gambar dengan berbagai properti */}
-                  <Image
-                    source={require("../assets/icon.png")}
-                    alt="Alternate Text"
-                    size="md"
-                    pt={2}
-                    rounded={10}
-                  />
-                  {/* Container untuk memberikan padding dan margin */}
-                  <Container pl={2} pb={2}>
-                    {/* Box untuk memberi margin dan padding */}
-                    <Box>
-                      {/* Text untuk menampilkan teks dengan berbagai properti */}
-                      <Text color={"#000000"}>ID Pesanan: {pesananId}</Text>
-                      <Text color={"#000000"}>
-                        Tanggal Pesanan: {dataPesanan.tanggal}
-                      </Text>
-                      <Text color={"#000000"}>Jenis: {dataPesanan.layanan}</Text>
-                      <Text color={"#000000"}>Jumlah Cucian: {dataPesanan.berat} Kg</Text>
-                      <Text color={"#000000"}>
-                        No Hp: {dataPesanan.noHp}
-                      </Text>
-                      <Text color={"#000000"}>Keterangan: {dataPesanan.keterangan}</Text>
-                      <Text color={"#000000"}>Total Harga: {dataPesanan.Harga}</Text>
-                    </Box>
-                  </Container>
-                </HStack>
+      {/* ScrollView untuk menggulirkan konten jika lebih panjang dari layar */}
+      <ScrollView flex={1}>
+        {/* VStack untuk menumpuk elemen-elemen secara vertikal */}
+        <VStack px={5} pt={6}>
+          {dataPesanan && (
+            <Box
+              bg={"#FFFF"}
+              rounded="xl"
+              overflow="hidden"
+              shadow={4}
+              borderColor="#0878CA"
+              borderWidth="1"
+              backgroundColor="#FFFF"
+              pt={2}
+              pr={4}
+              pb={2}
+              pl={4}
+              mb={3}
+            >
+              {/* Box untuk memberi margin dan padding */}
+              <Box ml={2} m={2}>
+                {/* Text bold untuk menampilkan teks dengan tebal */}
+                <Text bold>{dataPesanan.nama}</Text>
               </Box>
-            )}
-          </VStack>
-        </ScrollView>
-      </SafeAreaView>
+              {/* Divider untuk membuat garis pemisah */}
+              <Divider />
+
+              {/* HStack untuk menyusun elemen secara horizontal */}
+              <HStack px={2} py={2}>
+                {/* Image untuk menampilkan gambar dengan berbagai properti */}
+                <Image
+                  source={require("../assets/icon.png")}
+                  alt="Alternate Text"
+                  size="md"
+                  pt={2}
+                  rounded={10}
+                />
+                {/* Container untuk memberikan padding dan margin */}
+                <Container pl={2} pb={2}>
+                  {/* Box untuk memberi margin dan padding */}
+                  <Box>
+                    {/* Text untuk menampilkan teks dengan berbagai properti */}
+                    <Text color={"#000000"}>ID Pesanan: {pesananId}</Text>
+                    <Text color={"#000000"}>
+                      Tanggal Pesanan: {dataPesanan.tanggal}
+                    </Text>
+                    <Text color={"#000000"}>Jenis: {dataPesanan.layanan}</Text>
+                    <Text color={"#000000"}>
+                      Jumlah Cucian: {dataPesanan.berat} Kg
+                    </Text>
+                    <Text color={"#000000"}>No Hp: {dataPesanan.noHp}</Text>
+                    <Text color={"#000000"}>
+                      Keterangan: {dataPesanan.keterangan}
+                    </Text>
+                    <Text color={"#000000"}>
+                      Total Harga: {dataPesanan.Harga}
+                    </Text>
+                  </Box>
+                </Container>
+              </HStack>
+            </Box>
+          )}
+        </VStack>
+      </ScrollView>
     </>
   );
 };
---
+++ screens/Riwayat.js
@@ -0,0 +1,227 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  Divider,
+  ScrollView,
+  VStack,
+  Text,
+  HStack,
+  Button,
+  Box,
+  Modal,
+  Stack,
+  // Container,
+  // Heading,
+  // Center,
+  // NativeBaseProvider,
+} from "native-base";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  remove,
+  child,
+  // update,
+} from "firebase/database";
+import { Header } from "../components";
+
+const RiwayatPesanan = () => {
+  const navigation = useNavigation();
+  const [riwayatPesanan, setRiwayatPesanan] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPesanan, setSelectedPesanan] = useState(null);
+
+  // Menggunakan useEffect untuk melakukan fetch data ketika komponen di-mount
+  useEffect(() => {
+    const pesananRef = ref(getDatabase(), "Pesanan");
+
+    // Menggunakan onValue untuk mendapatkan data secara real-time
+    onValue(pesananRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        // Mengubah objek pesanan menjadi array
+        const pesananArray = Object.entries(data).map(([key, value]) => ({
+          id: key,
+          ...value,
+        }));
+        setRiwayatPesanan(pesananArray);
+      }
+    });
+  }, []);
+
+  // Fungsi untuk menghapus pesanan
+  const handleDelete = () => {
+    if (!selectedPesanan) return;
+
+    const pesananRef = ref(getDatabase(), "Pesanan");
+    const pesananKey = selectedPesanan.id;
+
+    // Menghapus pesanan dari database
+    remove(child(pesananRef, pesananKey))
+      .then(() => {
+        console.log("Pesanan berhasil dihapus!");
+        setShowModal(false);
+        // Secara opsional, Anda dapat memperbarui state lokal untuk mencerminkan perubahan
+        setRiwayatPesanan((prevPesanan) =>
+          prevPesanan.filter((pesanan) => pesanan.id !== pesananKey)
+        );
+      })
+      .catch((error) => {
+        console.error("Gagal menghapus pesanan:", error);
+      });
+  };
+
+  // Fungsi untuk menampilkan modal
+  const handleShowModal = (pesanan) => {
+    setSelectedPesanan(pesanan);
+    setShowModal(true);
+  };
+
+  return (
+    <>
+      <Header title={"Riwayat"} withBack="true" />
+
+      <ScrollView>
+        {/* Mencetak setiap pesanan dari array riwayatPesanan */}
+        {riwayatPesanan.map((pesanan) => (
+          <Box
+            key={pesanan.id}
+            mx={6}
+            my={3}
+            rounded="xl"
+            overflow="hidden"
+            
+            borderColor="#0878CA"
+            borderWidth="1"
+            backgroundColor="#FFFF"
+          >
+            <Stack p="2" space={3}>
+              <Box>
+                <HStack
+                  space={2}
+                  ml={2}
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                >
+                  {/* Text bold untuk menampilkan teks dengan tebal dan memanggil properti dari database nama */}
+                  <Text bold>{pesanan.nama}</Text>
+
+                  {/* TouchableOpacity dan Button untuk menampilkan tombol hapus */}
+                  <TouchableOpacity>
+                    <Button
+                      mx={4}
+                      mt={2}
+                      p={2}
+                      rounded="md"
+                      overflow="hidden"
+                      borderColor="#0878CA"
+                      borderWidth="1"
+                      backgroundColor="#0878CA"
+                      onPress={() => handleShowModal(pesanan)}
+                    >
+                      <Text
+                        color={"#FFFF"}
+                        fontSize={"sm"}
+                        bold
+                        onPress={() => handleShowModal(pesanan)}
+                      >
+                        Hapus
+                      </Text>
+                    </Button>
+                  </TouchableOpacity>
+                </HStack>
+
+                {/* Modal untuk konfirmasi penghapusan */}
+                <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+                  <Modal.Content
+                    borderWidth={1}
+                    borderColor={"#0878CA"}
+                    maxWidth="400px"
+                  >
+                    <Box mx={2}>
+                      <Modal.CloseButton />
+                      <Modal.Header>Hapus Riwayat</Modal.Header>
+
+                      {/* Modal.Footer dan Button untuk tombol Hapus */}
+                      <Modal.Footer>
+                        <Button.Group space={2}>
+                          <Button
+                            bg={"#0878CA"}
+                            borderRadius={"xl"}
+                            onPress={handleDelete}
+                          >
+                            <Text color={"#FFFF"}>Hapus</Text>
+                          </Button>
+                        </Button.Group>
+                      </Modal.Footer>
+                    </Box>
+                  </Modal.Content>
+                </Modal>
+              </Box>
+
+              <Box>
+                <Divider />
+                <HStack mx={4}>
+                  <Image
+                    source={require("../assets/icon.png")}
+                    alt="Alternate Text"
+                    size="md"
+                    borderColor={"#0878CA"}
+                    borderWidth={"1"}
+                    mt={4}
+                    mb={2}
+                    rounded={10}
+                  />
+
+                  <VStack mx={2}>
+                    <Box mt={2}>
+                      <Box p={3} mb={3}>
+                        {/* Text untuk menampilkan teks dengan berbagai properti */}
+                        <Text bold fontSize={16}>
+                          Tanggal: {pesanan.tanggal}
+                        </Text>
+                        <Text>Berat: {pesanan.berat}</Text>
+                        <Text>Harga: {pesanan.Harga}</Text>
+                      </Box>
+                    </Box>
+                  </VStack>
+                </HStack>
+
+                <HStack justifyContent={"flex-end"}>
+                  <Box>
+                    <Button
+                      p={2}
+                      mr={2}
+                      mb={2}
+                      rounded="md"
+                      overflow="hidden"
+                      bg={"#FFFF"}
+                      borderColor="#0878CA"
+                      borderWidth="1"
+                    >
+                      <TouchableOpacity
+                        onPress={() =>
+                          navigation.navigate("RincianPesanan", {
+                            pesananId: pesanan.id,
+                          })
+                        }
+                      >
+                        <Text bold color={"#0878CA"}>
+                          Rincian Pesanan
+                        </Text>
+                      </TouchableOpacity>
+                    </Button>
+                  </Box>
+                </HStack>
+              </Box>
+            </Stack>
+          </Box>
+        ))}
+      </ScrollView>
+    </>
+  );
+};
+
+export default RiwayatPesanan;

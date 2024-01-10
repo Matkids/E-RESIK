ff12f70633444110b10489b3abe081602715bee0
Matkids <90205897+Matkids@users.noreply.github.com>
2024-01-10 19:47:46 +0700

    Update E resik terbaharu
    Update fix

--- components/NativeBaseIcon.js
+++ components/NativeBaseIcon.js
@@ -1,3 +1,4 @@
+//nativebase
 import React from "react";
 import { Icon } from "native-base";
 import { G, Path } from "react-native-svg";
--- components/header.js
+++ components/header.js
@@ -3,6 +3,7 @@ import { Box, HStack, Image, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
 import {FontAwesome5} from "@expo/vector-icons";
 
+//Header
 const Header = ({}) => {
   const colour = "#FFFF";
   return (
@@ -25,15 +26,6 @@ const Header = ({}) => {
               </Text>
               <Text fontSize="md">Arif Rahmat Farrel Anggi Risa</Text>
             </VStack>
-          </HStack>
-          <HStack>
-            <TouchableOpacity>
-              <FontAwesome5
-                name="money-bill-wave"
-                size={24}
-                color="black"
-              />
-            </TouchableOpacity>
           </HStack>
         </HStack>
       </Box>
